08dd67cf04ad17a9d3c6d7573a6f7e9034508465
lrod <rleslie1015@gmail.com>
2019-04-11 12:51:13 -0700

    changed the syles on SearchBar to use styled-components

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,35 @@
 import React from "react";
 import "./SearchBar.css";
+import styled, { css, createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*, *::before, *::after {
+    .search-bar {
+    display: flex;
+    }
+    h1 {
+        font-family: 'Satisfy', cursive;
+        margin: 0 10px;
+}
+}`
+
+const H1 = styled.h1`
+    font-family: 'Satisfy', cursive;
+    margin: 0 10px;
+`
+const SearchBarDiv = styled.div`
+    display: flex;
+` 
 function SearchBar() {
     return (
-        <div className="search-bar">
+        <SearchBarDiv>
             <div> <i class="fab fa-instagram fa-3x"></i></div>  
-            <h1>instagram</h1>
+            <H1>instagram</H1>
             <div> <input type="text"></input></div>
             <div> <i class="far fa-compass fa-3x"></i></div>  
             <div><i class="far fa-heart fa-3x"></i></div>
             <div> <i class="fas fa-user fa-3x"></i> </div>
-        </div>
+        </SearchBarDiv>
       
     )
 }
